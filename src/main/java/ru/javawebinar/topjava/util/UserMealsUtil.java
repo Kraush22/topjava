fded37d8f78602ace8ba836c3b3ffa6dfd2099c1
Kraush22 <raooshan2017@gmail.com>
2018-07-01 14:35:25 +0500

    HW0 realization of the method getFilteredWithExceeded through stream and cycles

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -8,8 +8,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.*;
 import java.lang.Integer;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class UserMealsUtil {
@@ -23,7 +23,7 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000)
 
         );
-        List<UserMealWithExceed> result = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        List<UserMealWithExceed> result = getFilteredWithExceededStreams(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
 //        .toLocalDate();
 //        .toLocalTime();
 
@@ -33,14 +33,56 @@ public class UserMealsUtil {
 
     }
 
-    public static List<UserMealWithExceed> getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+    public static List<UserMealWithExceed> getFilteredWithExceededStreams(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        Collections.sort(mealList, new SortByUserMeal());
+        Map<LocalDate, Integer> calorControl = mealList.stream().collect(Collectors.groupingBy(userMeal -> convert(userMeal.getDateTime()), Collectors.summingInt(UserMeal::getCalories)));
+        List<UserMealWithExceed> index = new ArrayList<>();
+        mealList.stream()
+                .filter(x -> (x.getDateTime().toLocalTime().compareTo(startTime) >= 0 && x.getDateTime().toLocalTime().compareTo(endTime) <= 0))
+                .forEach(
+                        x -> {
+                            if (calorControl.get(x.getDateTime().toLocalDate()) > caloriesPerDay) {
+                                index.add(new UserMealWithExceed(x.getDateTime(), x.getDescription(), x.getCalories(), true));
+                            } else {
+                                index.add(new UserMealWithExceed(x.getDateTime(), x.getDescription(), x.getCalories(), false));
+                            }
+                        }
+                );
+
+        return index;
+    }
+
+    public static LocalDate convert(LocalDateTime ldt) {
+        int year = ldt.getYear();
+        int month = ldt.getMonthValue();
+        int day = ldt.getDayOfMonth();
+        return LocalDate.of(year, month, day);
+    }
+
+
+    public static List<UserMealWithExceed> getFilteredWithExceededCycles(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         List<UserMealWithExceed> index = new ArrayList<>();
         Collections.sort(mealList, new SortByUserMeal());
-        Map<LocalDate, Integer> calorControl = mealList.stream().collect(Collectors.groupingBy(UserMeal::getDateTime.toLocalDate(), Collectors.summingInt(UserMeal::getCalories)));
+        Map<LocalDate, Integer> calorControl = new HashMap<>();
 
-
-
-
-        return null;
+        for (UserMeal userMeal : mealList) {
+            if (!(calorControl.containsKey(userMeal.getDateTime().toLocalDate()))) {
+                calorControl.put(userMeal.getDateTime().toLocalDate(), userMeal.getCalories());
+            } else {
+                calorControl.put(userMeal.getDateTime().toLocalDate(), calorControl.get(userMeal.getDateTime().toLocalDate()) + userMeal.getCalories());
+            }
+        }
+        for (UserMeal userMeal : mealList) {
+            if (userMeal.getDateTime().toLocalTime().compareTo(startTime) >= 0 && userMeal.getDateTime().toLocalTime().compareTo(endTime) <= 0) {
+                if (calorControl.get(userMeal.getDateTime().toLocalDate()) > caloriesPerDay) {
+                    index.add(new UserMealWithExceed(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), true));
+                } else {
+                    index.add(new UserMealWithExceed(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), false));
+                }
+            }
+        }
+        return index;
     }
+
 }
+
